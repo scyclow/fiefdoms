baaabd1faab494ae708f3934226e4b2312323620
Julien Heller <flux627@users.noreply.github.com>
2022-12-29 03:32:17 -0500

    remove upgradeable; add support for hooks contract

---
+++ contracts/ERC721Hooks.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+import "./Dependencies.sol";
+
+interface IERC721Hooks {
+    function _setParent() external;
+    function _beforeTransfer(address from, address to, uint256 tokenId) external;
+    function _beforeMint(address to, uint256 tokenId) external;
+    function _beforeBurn(address from, uint256 tokenId) external;
+    function _beforeApprove(address to, uint256 tokenId) external;
+    function _beforeSetApprovalForAll(address operator, bool approved) external;
+}
+
+contract ERC721HooksBase is IERC721Hooks {
+    address public parent;
+
+    function _setParent() external {
+        require(parent == address(0), "Parent ERC721 can only be set upon creation");
+        parent = msg.sender;
+    }
+
+    modifier onlyParent() {
+        require(msg.sender == parent, "Only parent ERC721 can call hooks");
+        _;
+    }
+
+    function beforeTransfer(address from, address to, uint256 tokenId) internal virtual {}
+    function _beforeTransfer(address from, address to, uint256 tokenId) external onlyParent {
+        beforeTransfer(from, to, tokenId);
+    }
+
+    function beforeMint(address to, uint256 tokenId) internal virtual {}
+    function _beforeMint(address to, uint256 tokenId) external onlyParent {
+        beforeMint(to, tokenId);
+    }
+
+    function beforeBurn(address from, uint256 tokenId) internal virtual {}
+    function _beforeBurn(address from, uint256 tokenId) external onlyParent {
+        beforeBurn(from, tokenId);
+    }
+
+    function beforeApprove(address to, uint256 tokenId) internal virtual {}
+    function _beforeApprove(address to, uint256 tokenId) external onlyParent {
+        beforeApprove(to, tokenId);
+    }
+
+    function beforeSetApprovalForAll(address operator, bool approved) internal virtual {}
+    function _beforeSetApprovalForAll(address operator, bool approved) external onlyParent {
+        beforeSetApprovalForAll(operator, approved);
+    }
+}
--- contracts/ReferenceFiefdom.sol
+++ contracts/ReferenceFiefdom.sol
@@ -2,7 +2,8 @@
 
 
 
-import "./UpgradeableDependencies.sol";
+import "./BaseTokenURI.sol";
+import "./ERC721Hooks.sol";
 import "./Dependencies.sol";
 
 
@@ -17,39 +18,39 @@ interface ITokenURI {
   function tokenURI(uint256 tokenId) external view returns (string memory uri);
 }
 
-contract ReferenceFiefdom is Initializable, ERC721Upgradeable {
+contract ReferenceFiefdom is Initializable, ERC721 {
   using Strings for uint256;
 
   string public license;
+  IBaseContract public overlord;
+  ITokenURI public tokenURIContract;
+  IERC721Hooks public erc721Hooks;
   address public minter;
   uint256 public fiefdom;
-  IBaseContract public overlord;
-  address public tokenURIContract;
   bool public isActivated;
 
   uint256 private _totalSupply;
   uint256 private _maxSupply;
   string private _name;
   string private _symbol;
-
-  address private royaltyBeneficiary;
-  uint16 private royaltyBasisPoints;
+  bool private _isActivated;
+  address private _royaltyBeneficiary;
+  uint16 private _royaltyBasisPoints;
 
   event ProjectEvent(address indexed poster, string indexed eventType, string content);
   event TokenEvent(address indexed poster, uint256 indexed tokenId, string indexed eventType, string content);
 
   // This is only called when the reference contract is published
-  constructor() {
+  constructor() ERC721('', '') {
     initialize(msg.sender, 0);
   }
 
   // This is called by the proxy contract when *it* is published
   // Mints token 0 and does not set a name/symbol
   function initialize(address _overlord, uint256 _fiefdomTokenId) public initializer {
-    __ERC721_init(
-      string(abi.encodePacked('Fiefdom ', _fiefdomTokenId.toString())),
-      string(abi.encodePacked('FIEF', _fiefdomTokenId.toString()))
-    );
+    // Since constructor is not called (or called the first time with empty values)
+    _name = string(abi.encodePacked('Fiefdom ', _fiefdomTokenId.toString()));
+    _symbol = string(abi.encodePacked('FIEF', _fiefdomTokenId.toString()));
     overlord = IBaseContract(_overlord);
     fiefdom = _fiefdomTokenId;
 
@@ -62,8 +63,8 @@ contract ReferenceFiefdom is Initializable, ERC721Upgradeable {
     string memory name_,
     string memory symbol_,
     uint256 maxSupply_,
-    address tokenURIContract_,
-  ) external onlyOwner {
+    address tokenURIContract_
+  ) public onlyOwner {
     // Require that it can only be called once
     require(!isActivated);
 
@@ -76,20 +77,60 @@ contract ReferenceFiefdom is Initializable, ERC721Upgradeable {
 
     // Set the defailt minter address + ERC2981 royalty beneficiary
     minter = msg.sender;
-    royaltyBeneficiary = msg.sender;
-    royaltyBasisPoints = 1000;
+    _royaltyBeneficiary = msg.sender;
+    _royaltyBasisPoints = 1000;
 
     // Create a default TokenURI contract that points to a baseURI
 
-    tokenURIContract = tokenURIContract_;
+    tokenURIContract = ITokenURI(tokenURIContract_);
     isActivated = true;
 
     license = 'CC BY-NC 4.0';
 
     // Recover the 0th token
     _transfer(address(this), msg.sender, 0);
-
   }
+
+  function activateWitHooks(
+    string memory name_,
+    string memory symbol_,
+    uint256 maxSupply_,
+    address tokenURIContract_,
+    bytes memory hooksContractBytecode
+  ) public onlyOwner {
+    activate(name_, symbol_, maxSupply_, tokenURIContract_);
+    address _erc721Hooks;
+    assembly {
+      _erc721Hooks := create(0, add(hooksContractBytecode, 0x20), mload(hooksContractBytecode))
+    }
+    erc721Hooks = IERC721Hooks(_erc721Hooks);
+    erc721Hooks._setParent();
+  }
+
+
+  // Register hooks
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override {
+    if (address(erc721Hooks) == address(0)) return;
+    if (from != address(0) && to != address(0)) {
+      erc721Hooks._beforeTransfer(from, to, tokenId);
+    } else if (to != address(0)) {
+      erc721Hooks._beforeMint(to, tokenId);
+    } else if (from != address(0)) {
+      erc721Hooks._beforeBurn(from, tokenId);
+    }
+    revert("from and to are both zero");
+  }
+
+  function approve(address to, uint256 tokenId) public virtual override {
+    if (address(erc721Hooks) != address(0)) erc721Hooks._beforeApprove(to, tokenId);
+    super.approve(to, tokenId);
+  }
+
+  function setApprovalForAll(address operator, bool approved) public virtual override {
+    if (address(erc721Hooks) != address(0)) erc721Hooks._beforeSetApprovalForAll(operator, approved);
+    super.setApprovalForAll(operator, approved);
+  }
+
 
   // OWNERSHIP
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
@@ -112,11 +153,11 @@ contract ReferenceFiefdom is Initializable, ERC721Upgradeable {
 
   // VARIABLES
 
-  function name() public view virtual override(ERC721Upgradeable) returns (string memory) {
+  function name() public view virtual override(ERC721) returns (string memory) {
    return  _name;
   }
 
-  function symbol() public view virtual override(ERC721Upgradeable) returns (string memory) {
+  function symbol() public view virtual override(ERC721) returns (string memory) {
     return _symbol;
   }
 
@@ -156,7 +197,7 @@ contract ReferenceFiefdom is Initializable, ERC721Upgradeable {
   // Events
   function emitTokenEvent(uint256 tokenId, string calldata eventType, string calldata content) external {
     require(
-      owner() == _msgSender() || ERC721Upgradeable.ownerOf(tokenId) == _msgSender(),
+      owner() == _msgSender() || ERC721.ownerOf(tokenId) == _msgSender(),
       'Only project or token owner can emit token event'
     );
     emit TokenEvent(_msgSender(), tokenId, eventType, content);
@@ -169,15 +210,15 @@ contract ReferenceFiefdom is Initializable, ERC721Upgradeable {
 
   // Token URI
   function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-    address addr = tokenURIContract == address(0)
+    address addr = address(tokenURIContract) == address(0)
       ? overlord.defaultTokenURIContract()
-      : tokenURIContract;
+      : address(tokenURIContract);
 
     return ITokenURI(addr).tokenURI(tokenId);
   }
 
   function setTokenURIContract(address _tokenURIContract) external onlyOwner {
-    tokenURIContract = _tokenURIContract;
+    tokenURIContract = ITokenURI(_tokenURIContract);
   }
 
   // Contract owner actions
@@ -187,11 +228,11 @@ contract ReferenceFiefdom is Initializable, ERC721Upgradeable {
 
   // Royalty Info
   function setRoyaltyInfo(
-    address _royaltyBenificiary,
-    uint16 _royaltyBasisPoints
+    address royaltyBeneficiary,
+    uint16 royaltyBasisPoints
   ) external onlyOwner {
-    royaltyBeneficiary = _royaltyBenificiary;
-    royaltyBasisPoints = _royaltyBasisPoints;
+    _royaltyBeneficiary = royaltyBeneficiary;
+    _royaltyBasisPoints = royaltyBasisPoints;
   }
 
   function setMinter(address newMinter) external onlyOwner {
@@ -199,10 +240,10 @@ contract ReferenceFiefdom is Initializable, ERC721Upgradeable {
   }
 
   function royaltyInfo(uint256, uint256 _salePrice) external view returns (address, uint256) {
-    return (royaltyBeneficiary, _salePrice * royaltyBasisPoints / 10000);
+    return (_royaltyBeneficiary, _salePrice * _royaltyBasisPoints / 10000);
   }
 
-  function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Upgradeable) returns (bool) {
+  function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
     // ERC2981
     return interfaceId == bytes4(0x2a55205a) || super.supportsInterface(interfaceId);
   }
